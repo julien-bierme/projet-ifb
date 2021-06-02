a52120825471d241a22ecff8663adbf365af2706
juju-bm <75586362+juju-bm@users.noreply.github.com>
2021-06-02 21:56:38 +0200

    Add files via upload
    Changement pour le vidage du buffer (fonction clean_stdin(void))

--- Fonction Choix Modes.c
+++ Fonction Choix Modes.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 int modes(char* CHOIX, char* choix1,char* choix2,char* choix3);
+void clean_stdin();
 
 int main() {
     int c=0,a=0,b=0;
@@ -27,11 +28,10 @@ int main() {
 int modes(char* CHOIX,char* choix1,char* choix2,char* choix3) {
     int menu = 0;
     char keymenu;
-    char antibug;
 
     printf("%s - Choisir 1: %s 2: %s 3: %s \n",CHOIX,choix1,choix2,choix3);
     scanf("%c", &keymenu); // lecture du choix de l'utilisateur
-    antibug = getchar(); // vidage du buffer
+    clean_stdin();// vidage du buffer
     menu = atoi(&keymenu); // conversion du caractère en entier
 
     /*fonction qui répète l'opération tant que le choix n'est pas 1, 2 ou 3*/
@@ -40,7 +40,7 @@ int modes(char* CHOIX,char* choix1,char* choix2,char* choix3) {
             printf("Ecrire 1 2 ou 3 : \n");
             printf("%s - Choisir 1: %s 2: %s 3: %s \n",CHOIX,choix1,choix2,choix3);
             scanf("%c", &keymenu); // lecture du choix de l'utilisateur
-            antibug = getchar(); // vidage du buffer
+            clean_stdin();// vidage du buffer
             menu = atoi(&keymenu); // conversion du caractère en entier
         } while (menu < 1 || menu > 3);
     }
@@ -58,4 +58,12 @@ int modes(char* CHOIX,char* choix1,char* choix2,char* choix3) {
     }
     
     return menu;
+}
+void clean_stdin(void)
+{
+    int c;
+
+    do {
+        c = getchar();
+    } while (c != '\n' && c != EOF);
 }
