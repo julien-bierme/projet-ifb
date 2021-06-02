44ccd3ccef4aa72091ebde2887197ffa8f25e377
juju-bm <75586362+juju-bm@users.noreply.github.com>
2021-06-03 00:38:13 +0200

    Add files via upload

---
+++ Fonction Show Grid.c
@@ -0,0 +1,54 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+
+struct grid {
+    char grille[12][12];
+    int largeur;
+    int longeur
+};
+
+void debut_grille();
+void show_grid();
+
+int main(){
+    debut_grille();
+    show_grid();
+    return 0;
+}
+
+void debut_grille(void){
+    struct grid grille;
+
+    char glocal[12][12] = {" ~~~~~~~~~~"," ~~~~~~~~~~"," ~~~~~~~~~~"," ~~~~~~~~~~"," ~~~~~~~~~~"," ~~~~~~~~~~"," ~~~~~~~~~~"," ~~~~~~~~~~"," ~~~~~~~~~~"," ~~~~~~~~~~"," ~~~~~~~~~~"};
+
+    for (int i = 1; i < 11; i++)
+    {
+        for (int j = 1; j < 11; j++)
+        {
+            grille.grille[i][j]=glocal[i][j];
+        }
+    }
+
+}
+
+void show_grid(void){
+    struct grid grille;
+
+    for (int i = 1; i < 11; i++)
+    {
+        printf("%d",i);
+    }
+    printf("\n");
+    for (int i = 1; i < 11; i++)
+    {
+        for (int j = 1; j < 11; j++)
+        {
+            if(j==1){
+                printf("%d",i);
+            }
+            printf("%c",grille.grille[i][j]);
+        }
+        printf("\n");
+    }
+}
