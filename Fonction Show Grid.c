7818bc2b7af0c968000d2d18e77c8c21caf6f009
juju-bm <75586362+juju-bm@users.noreply.github.com>
2021-06-07 22:46:34 +0200

    Add files via upload
    Ajout des espaces

--- Fonction Show Grid.c
+++ Fonction Show Grid.c
@@ -35,19 +35,23 @@ void debut_grille(void){
 void show_grid(void){
     struct grid grille;
 
+    printf(" ");
     for (int i = 1; i < 11; i++)
     {
+        printf(" ");
         printf("%d",i);
     }
     printf("\n");
     for (int i = 1; i < 11; i++)
     {
+        printf("%d",i);
         for (int j = 1; j < 11; j++)
         {
-            if(j==1){
-                printf("%d",i);
+            if(j==1 && i!=10){
+                printf(" ");
             }
             printf("%c",grille.grille[i][j]);
+            printf(" ");
         }
         printf("\n");
     }
