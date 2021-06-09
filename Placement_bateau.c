24a5e83db8ed7d35c963c2340e54a5090023870f
Thomas Carré <83712496+ES0TERIX@users.noreply.github.com>
2021-06-09 02:39:47 +0200

    Add files via upload
    Voici la fonction qui fonctionne totalement plus que des réglaces et features à ajouter

--- Placement_bateau.c
+++ Placement_bateau.c
@@ -3,6 +3,7 @@
 #include <time.h>
 #include <string.h>
 
+/* structure type pour des bateaux*/
 typedef struct{
     char orientation;
     int taille;
@@ -12,61 +13,66 @@ typedef struct{
 ;
 
 int test(int taille, int abscisse, int ordonnee, char orientation);
+
 void matrice_affich(char matrice[10][10]);
 
-
+/* main gérant l'attribution des valeur aux bateaux */
 int main(){
     srand(time(0));
 
-    int taille, abscisse, ordonnee, retour;
+    int taille, abscisse, ordonnee, retour;  /* Init des Variables */
     char orientation;
-    char matrice_test[10][10];
+    char matrice_test[10][10]={0};
 
-    bateau boat[5];
-    for(int nb; nb<4; nb++){
-        taille = nb+1;
+    bateau boat[5];  /* 5 Unit Bateau */
+    
+    for(int nb=0; nb<4; nb++){  /* Bateau 2, 3, 4, 5 */
+        taille = nb+2;
         orientation = 'h';
         boat[nb].orientation ='h';
         boat[nb].taille=taille;
+        
         do{
-            abscisse = rand()%10;
+            abscisse = rand()%10;  /* randomise ordonnée et abscisse */
             ordonnee = rand()%10;
-            retour=test(taille, abscisse, ordonnee, orientation);
+            
+            retour=test(taille, abscisse, ordonnee, orientation); /* attribue une valeur à retour pour savoir si la fonction retour est ok */
         }while(retour==1);
-        boat[nb].abscisse=abscisse;
+        
+        boat[nb].abscisse=abscisse; /* attribue les valeur définitive d'un bateau */
         boat[nb].ordonnee=ordonnee;
 
-        
-        if (boat[nb].orientation =='h'){
-            for (int i; i==taille-1; i++){
-                matrice_test[boat[nb].ordonnee+i][boat[nb].abscisse]=taille;
+        if (boat[nb].orientation =='h'){  /* Pose les bateaux sur la matrice test */
+            for (int i=0; i < taille; i++){
+                matrice_test[boat[nb].ordonnee][boat[nb].abscisse+i] = taille+'0';
             }
 
         }else{
-            for (int i; i==taille-1; i++) {
-                matrice_test[boat[nb].ordonnee][boat[nb].abscisse + i] = taille;
+            for (int i=0; i < taille-1; i++) {
+                matrice_test[boat[nb].ordonnee+i][boat[nb].abscisse] = taille+'0';
             }
         }
 
-        printf("bateau n:%d, %c, %d, %d\n", nb, orientation, boat[nb].ordonnee, boat[nb].abscisse);
+        printf("bateau n:%d, %c, %d, %d\n", nb, orientation, boat[nb].ordonnee, boat[nb].abscisse); /* Print les infos du bateau */
     }
-    void matrice_affich(char matrice_test[10][10]);
+    
+    matrice_affich(matrice_test);
     return 0;
 }
 
-int test(int taille, int abscisse, int ordonnee, char orientation){
-    int retour;
-    char matrice[10][10];
-    for (int i; i==taille; i++){
+int test(int taille, int abscisse, int ordonnee, char orientation){  /* vérifie possibibilité poser bateau */
+    int retour=0;
+    char matrice[10][10]={0};
+    for (int i = 0; i < taille; i++){
         if (orientation=='v'){
-            if (ordonnee+i<11 && matrice[ordonnee+i][abscisse]=='\0'){
+            if (ordonnee+i<11 && matrice[ordonnee+i][abscisse]==0){
                 matrice[ordonnee+i][abscisse]=taille;
             }else{
                 retour=1;
             }
         }
         else if (orientation=='h'){
-            if(abscisse+i<11 && matrice[ordonnee][abscisse+i]=='\0'){
+            if(abscisse+i<11 && matrice[ordonnee][abscisse+i]==0){
                 matrice[ordonnee][abscisse+i]=taille;
             }else{
                 retour=1;
@@ -76,13 +82,19 @@ int test(int taille, int abscisse, int ordonnee, char orientation){
 
         }
     }
+    return retour;
 }
 
-void matrice_affich(char matrice[10][10]) {
+void matrice_affich(char matrice[10][10]){
+    printf("test\n");
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
-            printf("%c ", matrice[i][j]);
+            if (matrice[i][j]==0){
+                printf("x ");
+            }else{
+                printf("%c ",matrice[i][j]);
+            }   
         }
-        printf("/n");
+        printf("\n");
     }
 }
